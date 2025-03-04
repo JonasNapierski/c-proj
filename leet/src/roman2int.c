a7b5b5a49ba99bde41fb2ebbaab6b1225d08cbd6
Jonas Napierski <pudeljoni@gmail.com>
2025-03-04 21:10:24 +0100

    finish roman2int

--- leet/src/roman2int.c
+++ leet/src/roman2int.c
@@ -1,4 +1,5 @@
 #include "../include/roman2int.h"
+#include <stdint.h>
 
 int romanToInt(char *s) {
     int max_roman = 7;
@@ -6,13 +7,13 @@ int romanToInt(char *s) {
     char symbols[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int values[] = {1,5,10,50,100,500,1000};
     
-    char pre_idx = 0;
+    int pre_idx = -1;
     while (*s != '\0') {
         int i = 0;
         while (i < max_roman && *s != symbols[i]) i++;
 
         if (i < max_roman) {
-            if (i > pre_idx) {
+            if (i > pre_idx && pre_idx != -1) {
                 result = result -(2*values[pre_idx]);
             }
             result += values[i];
