02a629c52dc24110ac7847865d829b8ca77d1c64
Jonas Napierski <pudeljoni@gmail.com>
2025-04-08 22:29:31 +0200

    detect amount of columns

--- leet/main.c
+++ leet/main.c
@@ -11,11 +11,33 @@ int main() {
 }
 */
 
+#define BUFFER_SIZE 32
+
 
 int main() {
     FILE *csv_file = fopen("./test.csv", "r");
     size_t ret;
-    unsigned char buffer[csv_file]; 
-    fread(void *, unsigned long, unsigned long, csv_file);
-    printf("Hello World");
+    unsigned char buffer[BUFFER_SIZE]; 
+    size_t succ_read = fread(buffer, sizeof(*buffer), BUFFER_SIZE, csv_file);
+
+    // ID,NAME,VALUE
+    char *header[3] = {"ID", "NAME", "VALUES" };
+    char ***values;
+
+    int i = 0;
+    
+    int count = 0;
+    while(buffer[i] != '\n')
+    {
+        if (',' == buffer[i]) count++;
+
+        i++;
+    }
+
+    if (count != 0) count++;
+
+    printf("Columns: %i\n", count);
+    
+    printf("Hello World\n");
+    fclose(csv_file);
 }
