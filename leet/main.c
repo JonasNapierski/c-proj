baa8e5fd9a22c606ad0af24c9ece1a550aec5fd4
Jonas Napierski <pudeljoni@gmail.com>
2025-04-09 07:55:40 +0200

    remove old main code

--- leet/main.c
+++ leet/main.c
@@ -1,15 +1,5 @@
 #include<stdbool.h>
 #include <stdio.h>
-// #include "include/palindrom.h"
-
-
-/*
-int main() {
-    bool isPal = isPalindrome(-121);
-    int r = reverse(123);
-    printf("Hello WOrld");
-}
-*/
 
 #define BUFFER_SIZE 32
 
