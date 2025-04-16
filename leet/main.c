1c7b030e581203284f84da27c5d165db4d824ee4
Jonas Napierski <pudeljoni@gmail.com>
2025-04-16 22:21:14 +0200

    Seg fault for char *s[]

--- leet/main.c
+++ leet/main.c
@@ -1,33 +1,74 @@
 #include<stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define BUFFER_SIZE 32
+#define BUFFER_SIZE 4096
 
 
 int main() {
+    printf("Hello World\n");
     FILE *csv_file = fopen("./test.csv", "r");
     size_t ret;
-    unsigned char buffer[BUFFER_SIZE]; 
+    char buffer[BUFFER_SIZE]; 
     size_t succ_read = fread(buffer, sizeof(*buffer), BUFFER_SIZE, csv_file);
 
     // ID,NAME,VALUE
-    char *header[3] = {"ID", "NAME", "VALUES" };
-    char ***values;
-
     int i = 0;
     
-    int count = 0;
+    int columns = 0;
+    int rows = 0;
     while(buffer[i] != '\n')
     {
-        if (',' == buffer[i]) count++;
+        if (',' == buffer[i]) columns++;
 
         i++;
     }
 
-    if (count != 0) count++;
+    if (columns != 0) columns++;
+    if (rows < 0) rows--;
 
-    printf("Columns: %i\n", count);
-    
     printf("Hello World\n");
+    char **header = (char**)malloc(sizeof(char*) * columns);
+
+    if (header == NULL) return -1;
+    char *valeus[columns][rows]; 
+
+    i = 0;
+    int column_cursor = 0;
+    int column_start = 0;
+
+    while(buffer[i] != '\n')
+    {
+        if ( ',' == buffer[i]) {
+            char *header_name = (char*)malloc(sizeof(char)* (i- column_start));
+
+            //strncpy((header[column_cursor]), (buffer+column_start), i - column_start);
+            *(header+column_cursor) = header_name;
+            column_cursor++;
+            printf("This got triggered\n");
+            column_start = i+1;
+        }
+        i++;
+    }
+    char *header_name = (char*)malloc(sizeof(char)* (i- column_start));
+
+    *(header+column_cursor) = header_name;
+    column_cursor++;
+    printf("This got triggered\n");
+    column_start = i+1;
+
+    printf("Columns: %i\n", columns);
+    
+    printf("Clean up memory after use\n");
+    for (int e = 0; e < columns; e++)
+    {
+        printf("free %i\n", e);
+        free((header+e));
+    }
+    free(header);
+    printf("End of cleaning\n");
+    
     fclose(csv_file);
+    return 0;
 }
