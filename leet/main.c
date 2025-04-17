71922f661351350d9e33548a81ce96c97ccc824f
Jonas Napierski <pudeljoni@gmail.com>
2025-04-17 11:48:17 +0200

    fix core dump and sugmentation error

--- leet/main.c
+++ leet/main.c
@@ -7,13 +7,22 @@
 
 
 int main() {
-    printf("Hello World\n");
+    printf("Start loading file './test.csv'\n");
     FILE *csv_file = fopen("./test.csv", "r");
+
+    if (!csv_file) {
+        perror("fopen failed. This may be caused due to missing permission or an non existing file.");
+        return EXIT_FAILURE;
+    }
+
     size_t ret;
     char buffer[BUFFER_SIZE]; 
     size_t succ_read = fread(buffer, sizeof(*buffer), BUFFER_SIZE, csv_file);
 
-    // ID,NAME,VALUE
+
+    printf("Close file. Content read into buffer\n");
+    fclose(csv_file);
+
     int i = 0;
     
     int columns = 0;
@@ -28,47 +37,46 @@ int main() {
     if (columns != 0) columns++;
     if (rows < 0) rows--;
 
-    printf("Hello World\n");
+    printf("Columns: %i\n", columns);
     char **header = (char**)malloc(sizeof(char*) * columns);
 
-    if (header == NULL) return -1;
+    if (header == NULL) return EXIT_FAILURE;
     char *valeus[columns][rows]; 
 
     i = 0;
     int column_cursor = 0;
     int column_start = 0;
 
+    char *header_name;
     while(buffer[i] != '\n')
     {
         if ( ',' == buffer[i]) {
-            char *header_name = (char*)malloc(sizeof(char)* (i- column_start));
+
+            header_name = (char*)malloc(sizeof(char)* (i- column_start));
 
             //strncpy((header[column_cursor]), (buffer+column_start), i - column_start);
-            *(header+column_cursor) = header_name;
+            header[column_cursor] = header_name;
             column_cursor++;
-            printf("This got triggered\n");
+            printf("Create memory for: %i - %i (size: %i)\n", column_start, i, (i - column_start));
             column_start = i+1;
         }
         i++;
     }
-    char *header_name = (char*)malloc(sizeof(char)* (i- column_start));
+    printf("Create memory for: %i - %i (size: %i)\n", column_start, i, (i - column_start));
+    header_name = (char*)malloc(sizeof(char)* (i - column_start));
 
     *(header+column_cursor) = header_name;
-    column_cursor++;
-    printf("This got triggered\n");
-    column_start = i+1;
+    column_cursor=0;
 
-    printf("Columns: %i\n", columns);
     
     printf("Clean up memory after use\n");
     for (int e = 0; e < columns; e++)
     {
         printf("free %i\n", e);
-        free((header+e));
+        free(header[e]);
     }
     free(header);
     printf("End of cleaning\n");
     
-    fclose(csv_file);
     return 0;
 }
